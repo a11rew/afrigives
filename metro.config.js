cf78611219715cc4f17e89255f69f06f32426faa
Andrew Glago <andrewglago1@gmail.com>
2022-02-01 19:47:59 +0000

    Update metro config for Expo

--- metro.config.js
+++ metro.config.js
@@ -1,14 +1,16 @@
 /* eslint-disable import/no-extraneous-dependencies */
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { getDefaultConfig } = require('metro-config');
+const { getDefaultConfig } = require('expo/metro-config');
 
+// extra config is needed to enable `react-native-svg-transformer`
 module.exports = (async () => {
   const {
     resolver: { sourceExts, assetExts },
-  } = await getDefaultConfig();
+  } = await getDefaultConfig(__dirname);
   return {
     transformer: {
       babelTransformerPath: require.resolve('react-native-svg-transformer'),
+      assetPlugins: ['expo-asset/tools/hashAssetFiles'],
     },
     resolver: {
       assetExts: assetExts.filter((ext) => ext !== 'svg'),
