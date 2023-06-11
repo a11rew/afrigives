e41f358103232996d745cfce6148327e201936fd
Andrew Glago <andrewglago1@gmail.com>
2023-06-11 16:21:33 +0000

    fix: mobile dev env errors

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -1,5 +1,10 @@
 module.exports = function (api) {
   api.cache(true);
+
+  // Make Expo Router run from `src/app` instead of `app`.
+  // Path is relative to `/node_modules/expo-router`
+  process.env.EXPO_ROUTER_APP_ROOT = '../../apps/mobile/src/app';
+
   return {
     presets: ['babel-preset-expo'],
     plugins: [
--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,20 +1,39 @@
 /* eslint-disable import/no-extraneous-dependencies */
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
+
+const projectRoot = __dirname;
+const workspaceRoot = path.resolve(projectRoot, '../..');
+
+const config = getDefaultConfig(projectRoot);
+
+const { transformer, resolver } = config;
+
+// Add the additional `cjs` extension to the resolver
+config.resolver.sourceExts.push('cjs');
+
+// 1. Watch all files within the monorepo
+config.watchFolders = [workspaceRoot];
+// 2. Let Metro know where to resolve packages and in what order
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, 'node_modules'),
+  path.resolve(workspaceRoot, 'node_modules'),
+];
+// 3. Force Metro to resolve (sub)dependencies only from the `nodeModulesPaths`
+// config.resolver.disableHierarchicalLookup = true;
 
 // extra config is needed to enable `react-native-svg-transformer`
-module.exports = (async () => {
-  const {
-    resolver: { sourceExts, assetExts },
-  } = await getDefaultConfig(__dirname);
-  return {
-    transformer: {
-      babelTransformerPath: require.resolve('react-native-svg-transformer'),
-      assetPlugins: ['expo-asset/tools/hashAssetFiles'],
-    },
-    resolver: {
-      assetExts: assetExts.filter((ext) => ext !== 'svg'),
-      sourceExts: [...sourceExts, 'svg'],
-    },
-  };
-})();
+config.transformer = {
+  ...transformer,
+  babelTransformerPath: require.resolve('react-native-svg-transformer'),
+  assetPlugins: ['expo-asset/tools/hashAssetFiles'],
+};
+
+config.resolver = {
+  ...resolver,
+  assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
+  sourceExts: [...resolver.sourceExts, 'svg'],
+};
+
+module.exports = config;
