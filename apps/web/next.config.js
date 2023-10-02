2e00667e9104c02a7051ab3755e5c209c45b7684
Andrew Glago <andrewglago1@gmail.com>
2023-10-02 12:49:17 +0000

    fix(web): support image serving

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["afrigives.a11rew.dev"],
+    imageSizes: [382, 1920],
+  },
 };
 
-module.exports = nextConfig;
-
 module.exports = {
+  ...nextConfig,
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
